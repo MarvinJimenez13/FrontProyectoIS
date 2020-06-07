7a8825fbcd2a62d909d04cc4ca0c188968599871
MarvinJimenez13 <marvinjiga@gmail.com>
2020-06-07 00:14:38 -0500

    Controlador de registro de Resistencias con Angular.

---
+++ controladores/ControladorHome.js
@@ -0,0 +1,20 @@
+var myApp = angular.module("Home", []);
+var home = function($scope, $http){
+
+    $scope.registro = {};
+    $scope.link = "index.html#registroPivote";
+    var urlAPI = 'http://localhost:3000/resistencia';
+    $scope.registrarResistencia = function(registro){
+        $scope.registro.nominal = document.getElementById("valorNominal").value;
+        $scope.registro.tolerancia = document.getElementById("tolerancia").value;
+        console.log($scope.registro);
+        $http.post(urlAPI, JSON.stringify($scope.registro), { headers : {'Content-Type': 'application/json'} })
+			.then(function(response) {
+			   alert(response.data);
+			   window.location.href = $scope.link;
+			});
+    }
+
+}
+
+myApp.controller("registro", ['$scope', '$http', home]);
