a9467457969b922419f50b3892c1f3e695de20da
MarvinJimenez13 <marvinjiga@gmail.com>
2020-06-08 22:40:51 -0500

    Gestión de resistencias en desarrollo.

---
+++ controladores/ControladorGestion.js
@@ -0,0 +1,6 @@
+var myApp = angular.module("Gestion", []);
+var gestion = function($scope, $http, SessionService){
+
+}
+
+myApp.controller("gestion", ['$scope', '$http', 'SessionService', gestion]);
