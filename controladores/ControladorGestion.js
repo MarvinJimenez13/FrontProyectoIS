ccbe1272c42bd31b11290408fbc751109abba1a7
MarvinJimenez13 <marvinjiga@gmail.com>
2020-06-12 00:51:44 -0500

    Mostrar resistencias registradas.

--- controladores/ControladorGestion.js
+++ controladores/ControladorGestion.js
@@ -1,6 +1,13 @@
-var myApp = angular.module("Gestion", []);
-var gestion = function($scope, $http, SessionService){
+var myApp = angular.module("Gestion", ['Session']);
 
+var gestion = function($scope, $http, SessionService){
+    $scope.cargarResistencias = function(){
+        $http.get('http://localhost:3000/resistencias').then(function(response){
+            $scope.resistencias = response.data;
+            console.log($scope.resistencias);
+        });
+    }
+    $scope.cargarResistencias();
 }
 
 myApp.controller("gestion", ['$scope', '$http', 'SessionService', gestion]);
